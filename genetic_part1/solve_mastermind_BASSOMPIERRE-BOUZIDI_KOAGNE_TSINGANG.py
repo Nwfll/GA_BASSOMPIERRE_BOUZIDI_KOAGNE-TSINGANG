4f3665d7f1a1d4201b423015815c7c5685b633b6
Bouzidi Nawfel <nawfel.bouzidi@epfedu.fr>
2025-02-25 16:19:58 +0100

    exo1

--- genetic_part1/solve_mastermind_BASSOMPIERRE-BOUZIDI_KOAGNE_TSINGANG.py
+++ genetic_part1/solve_mastermind_BASSOMPIERRE-BOUZIDI_KOAGNE_TSINGANG.py
@@ -7,6 +7,11 @@ Created on Mon Feb 21 11:24:15 2022
 Template for exercise 1
 (genetic algorithm module specification)
 """
+import mastermind as mm
+import random
+
+
+MATCH = mm.MastermindMatch(secret_size=4)
 
 
 class Individual:
@@ -31,6 +36,7 @@ class Individual:
         return f'Indiv({self.fitness:.1f},{self.chromosome})'
 
 
+
 class GASolver:
     def __init__(self, selection_rate=0.5, mutation_rate=0.1):
         """Initializes an instance of a ga_solver for a given GAProblem
@@ -45,6 +51,12 @@ class GASolver:
 
     def reset_population(self, pop_size=50):
         """ Initialize the population with pop_size random Individuals """
+        for i in range(pop_size): 
+            chromosome = MATCH.generate_random_guess()
+            fitness = MATCH.rate_guess(chromosome)
+            new_individual = Individual(chromosome, fitness)
+            self._population.append(new_individual)
+
         pass  # REPLACE WITH YOUR CODE
 
     def evolve_for_one_generation(self):
@@ -57,6 +69,31 @@ class GASolver:
                 mutation_rate i.e., mutate it if a random value is below   
                 mutation_rate
         """
+        size_required = len(self._population)
+        self._population.sort(key=lambda ind: ind.fitness, reverse = True)
+        del self._population[int(len(self._population)*(1-self._selection_rate)):]  #selection rate stand for the people remaining so in order
+        # to delete we use 1-selection_rate
+
+        number_of_birth_needed = size_required-len(self._population) #we want to create as much inidividuals as the ones deleted
+        
+        for baby in range(number_of_birth_needed):
+            parent1 = self._population[baby] 
+            parent2 = self._population[baby+1] # elitist behaviour: we make reproduce only the best individuals #KIFFEUR 
+            crossing_point = random.randrange(0, len(parent1.chromosome))
+            new_chromosome = parent1.chromosome[0:crossing_point] + parent2.chromosome[crossing_point:] #we concatenate the chromosomes of each parent
+
+
+            #MUTATION
+            number = random.random()
+            if number < self._mutation_rate:
+                valid_colors = mm.get_possible_colors()
+                new_gene = random.choice(valid_colors) #we randomly chose a new mutation color
+                gene_mutated = random.randrange(0, len(new_chromosome)) #we randomly select the gene mutated
+                new_chromosome[gene_mutated] = new_gene #we apply the color to the new gene selected
+            fitness=MATCH.rate_guess(new_chromosome)
+            new_individual = Individual(new_chromosome, fitness) #we create a new individual
+            self._population.append(new_individual)
+
         pass  # REPLACE WITH YOUR CODE
 
     def show_generation_summary(self):
@@ -65,6 +102,7 @@ class GASolver:
 
     def get_best_individual(self):
         """ Return the best Individual of the population """
+        return self._population[0]
         pass  # REPLACE WITH YOUR CODE
 
     def evolve_until(self, max_nb_of_generations=500, threshold_fitness=None):
@@ -73,4 +111,17 @@ class GASolver:
             - The fitness of the best Individual is greater than or equal to
               threshold_fitness
         """
+        nb_of_generations = 0
+        self._population.sort(key=lambda ind: ind.fitness, reverse = True)
+        while nb_of_generations < max_nb_of_generations and self._population[0].fitness < threshold_fitness:
+            self.evolve_for_one_generation()
+
         pass  # REPLACE WITH YOUR CODE
+
+#we run a game
+solver = GASolver()
+solver.reset_population()
+solver.evolve_until(threshold_fitness=MATCH.max_score())
+best = solver.get_best_individual()
+print(f"Best guess {best.chromosome}")
+print(f"Problem solved? {MATCH.is_correct(best.chromosome)}")
