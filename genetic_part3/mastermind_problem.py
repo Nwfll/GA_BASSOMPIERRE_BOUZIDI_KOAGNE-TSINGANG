33981cd760bc11e048001139173a1bae8c11ceb3
bassompierre <benoit.bassompierre@epfedu.fr>
2025-03-21 15:20:42 +0100

    adding the use of GASolver for the mastermind problem

--- genetic_part3/mastermind_problem.py
+++ genetic_part3/mastermind_problem.py
@@ -1,33 +1,46 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Feb 18 2022
-
-@author: tdrumond & agademer
-
-Template file for your Exercise 3 submission 
-(GA solving Mastermind example)
+Example of implementation of the GA Solver package to solve the Traveling Salesperson Problem.
 """
-from ga_solver import GAProblem
+from ga_solver import GAProblem, GASolver
 import mastermind as mm
+import random
 
 
 class MastermindProblem(GAProblem):
-    """Implementation of GAProblem for the mastermind problem"""
-    pass  # REPLACE WITH YOUR CODE
+    """Implementation of GAProblem for the Mastermind problem."""
+
+    def __init__(self, match=None):
+        """Initialize the problem with a MastermindMatch instance."""
+        self.match = match if match else mm.MastermindMatch()
+
+    def create_chromosome(self):
+        """Creates a random chromosome as a list."""
+        return self.match.generate_random_guess()
+
+    def fitness(self, chromosome):
+        """Evaluates how close the chromosome is to the correct solution."""
+        return self.match.rate_guess(chromosome)
+
+    def cross_population(self, parent1: list, parent2: list):
+        """Performs one-point crossover."""
+        crossing_point = random.randrange(0, len(parent1))
+        return parent1[:crossing_point] + parent2[crossing_point:]
+
+    def mutation(self, chromosome: list):
+        """Mutates a random gene in the chromosome."""
+        valid_colors = mm.get_possible_colors()
+        gene_mutated = random.randrange(0, len(chromosome))
+        chromosome[gene_mutated] = random.choice(valid_colors)
+        return chromosome
 
 
 if __name__ == '__main__':
-
-    from ga_solver import GASolver
-
-    match = mm.MastermindMatch(secret_size=6)
-    problem = MastermindProblem(match)
+    match = mm.MastermindMatch()
+    problem = MastermindProblem(match)  
     solver = GASolver(problem)
 
     solver.reset_population()
     solver.evolve_until()
 
-    print(
-        f"Best guess {mm.decode_guess(solver.getBestDNA())} {solver.get_best_individual()}")
-    print(
-        f"Problem solved? {match.is_correct(solver.get_best_individual().chromosome)}")
+    print(f"Best guess {mm.encode_guess(solver.get_best_individual().chromosome)} {solver.get_best_individual()}")
+    print(f"Problem solved? {match.is_correct(solver.get_best_individual().chromosome)}")
